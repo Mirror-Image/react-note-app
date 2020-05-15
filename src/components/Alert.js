d5199a54becd32f9776711b0f669f4c2a31497f3
Maksim Borovik <borovik.maksim@gmail.com>
2020-05-16 00:32:45 +0300

    Added an animation to our app

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,21 +1,33 @@
 import React, {useContext} from "react";
+import {CSSTransition} from 'react-transition-group'
 import {AlertContext} from "../context/alert/alertContext";
 
 
 export const Alert = () => {
   const {alert, hide} = useContext(AlertContext)
 
-  if (!alert.visible) {
+  /*if (!alert.visible) {
     return null;
-  }
+  }*/
 
   return (
-    <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
-      <strong>{alert.type === 'danger' ? 'Danger!' : alert.type ? 'Success!' : 'Warning!'}</strong>
-      {alert.text}
-      <button onClick={hide} type="button" className="close" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+    <CSSTransition
+      in={alert.visible}
+      timeout={{
+        enter: 500,
+        exit: 350
+      }}
+      classNames={'alert'}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div className={`alert alert-${alert.type || 'warning'} alert-dismissible`}>
+        <strong>{alert.type === 'danger' ? 'Danger!' : alert.type ? 'Success!' : 'Warning!'}</strong>
+        {alert.text}
+        <button onClick={hide} type="button" className="close" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </CSSTransition>
   )
 }
